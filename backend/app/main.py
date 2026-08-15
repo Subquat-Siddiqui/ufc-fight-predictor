2fabd2a827ff7f7631967574a3b25c92aef6272c
Subquat Siddiqui <subquatsiddiqui@gmail.com>
2026-08-15 14:25:34 -0400

    updated port to have frontend display during testing

--- backend/app/main.py
+++ backend/app/main.py
@@ -9,7 +9,7 @@ app = FastAPI(title="UFC Fight Predictor API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # For when frontend is implemented and running on a different port
+    allow_origins=["http://localhost:5173"],  # For when frontend is implemented and running on a different port
     allow_methods=["*"],
     allow_headers=["*"],
 )
